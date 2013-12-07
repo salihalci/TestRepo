5226a8b261d432e7e51108413a710d3d16aacaa4
IS96139 <IS96139@T1LPRVT1346.gm.isbank.com.tr>
2013-12-07 14:42:29 -0430

    initial

---
+++ TestVaadin/src/com/sa/test/Test.java
@@ -0,0 +1,27 @@
+package com.sa.test;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+
+
+@Title("Hello Window")
+public class Test extends UI {
+    @Override
+    protected void init(VaadinRequest request) {
+        // Create the content root layout for the UI
+        VerticalLayout content = new VerticalLayout();
+        setContent(content);
+
+        // Display the greeting
+        content.addComponent(new Label("Hello World!"));
+    }
+   
+}
+
+
+
